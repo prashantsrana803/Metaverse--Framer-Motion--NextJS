20193f08540c3cd8cffc55abb3cf6bb7ac40eb8e
Prashant Pratap Singh <prashantsrana803@gmail.com>
2024-09-05 15:52:28 +0530

    Metaverse of Framer-Motion
    Created ComstomTexts, Navbar, Hero Section, along with layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{html,js,jsx}',
+    './components/**/*.{html,js,jsx}',
+    './sections/**/*.{html,js,jsx}',
+    './styles/**/*.{js,jsx}',
   ],
+  mode: 'jit',
   theme: {
     extend: {
+      colors: {
+        'primary-black': '#1A232E',
+        'secondary-white': '#c7c7c7',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
