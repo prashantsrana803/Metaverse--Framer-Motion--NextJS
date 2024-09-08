c5d5b0861cbcc81e4f0d6ad264daa012b5c23015
Prashant Pratap Singh <prashantsrana803@gmail.com>
2024-09-08 18:31:50 +0530

    color changed for numbers in start steps

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -2,7 +2,7 @@ import styles from "@/styles";
 
 const StartSteps = ({ number, text }) => (
   <div className={`${styles.flexCenter} flex-row`}>
-    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#322F5D]`}>
+    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}>
       <p className="font-bold text-[20px] text-white">
         {number}
       </p>
